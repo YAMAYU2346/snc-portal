d38498494b97106fa79087f6a3034f29e8b5f769
Yuya Yamada <yuya350@gmail.com>
2022-04-09 09:07:55 +0900

    dev

--- firebase/firestore.ts
+++ firebase/firestore.ts
@@ -1,21 +1,20 @@
 import { collection, getDocs, getFirestore } from 'firebase/firestore'
 import '../firebase/clientApp' // Initialize FirebaseApp
 
-export type Event = {
+export type Game = {
   id: string
   name: string
-  data: string
 }
 
-export async function getEvents(): Promise<Event[]> {
-  const events = new Array<Event>()
+export async function getGames(): Promise<Game[]> {
+  const games = new Array<Game>()
   const db = getFirestore()
-  const querySnapshot = await getDocs(collection(db, '/events'))
+  const querySnapshot = await getDocs(collection(db, '/games'))
 
   querySnapshot.forEach((doc) => {
-    const event = doc.data() as Event
-    events.push({ ...event, id: doc.id })
+    const game = doc.data() as Game
+    games.push({ ...game, id: doc.id })
   })
 
-  return events
+  return games
 }
